a5578940997fcc61353d54a3afc0bf35aba819db
Willie <whweb@aon.at>
2018-03-15 20:01:32 +0100

    Ein versuch aus AS

--- helloworld/src/main/java/at/technikumwien/helloworld/ui/main/MainActivity.java
+++ helloworld/src/main/java/at/technikumwien/helloworld/ui/main/MainActivity.java
@@ -1,6 +1,5 @@
 package at.technikumwien.helloworld.ui.main;
-
-
+/* DER einsprungpunkt */
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
